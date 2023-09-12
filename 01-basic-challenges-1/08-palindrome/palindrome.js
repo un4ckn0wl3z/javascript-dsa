4e0f99ed8a75b4bda6c517fc316cfa15552efcaf
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-12 21:43:16 +0700

    feat: solved palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,7 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    const alphanum = str.replace(/[^a-z0-9]/gi, '');
+    const reversed = alphanum.split('').reverse().join('')
+    return alphanum.toLowerCase() === reversed.toLowerCase()
+}
 
 module.exports = isPalindrome;
