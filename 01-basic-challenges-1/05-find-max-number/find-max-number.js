c46563d5d126a40a62eb71dd26c145a30927f334
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-12 20:29:30 +0700

    feat: solved find-max-number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,9 @@
-function findMaxNumber() {}
+function findMaxNumber(nums) {
+    let max = nums[0];
+    nums.forEach(n => {
+        if (n > max) max = n
+    })
+    return max
+}
 
 module.exports = findMaxNumber;
