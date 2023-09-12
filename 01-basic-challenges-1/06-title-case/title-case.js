d9656159f9ff34b162d86ec229d9ebcd5713f43a
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-12 21:03:01 +0700

    feat: solved title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,24 @@
-function titleCase() {}
+function titleCase(str) {
+    const lower = str.toLowerCase()
+    let result = ''
+    for(i=0; i<lower.length;i++){
+        if(i === 0){
+            result += lower[i].toUpperCase()
+        }else if(lower[i-1] === ' '){
+            result += lower[i].toUpperCase()
+        }else {
+            result += lower[i]
+        }
+    }
+    return result
+}
 
-module.exports = titleCase;
+function titleCase2(str) {
+    const words = str.toLowerCase().split(' ')
+    for(i=0;i<words.length;i++){
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ')
+}
+
+module.exports = titleCase2;
