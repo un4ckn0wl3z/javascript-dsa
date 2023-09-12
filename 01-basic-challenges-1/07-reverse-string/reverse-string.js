eca102c45cc153aaebb3f5c79b86579067548164
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-12 21:15:44 +0700

    feat: solved reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,13 @@
-function reverseString() {}
+function reverseString(str) {
+    let result = '';
+    for(i=str.length-1;i>=0;i--){
+        result += str[i]
+    }
+    return result
+}
 
-module.exports = reverseString;
+function reverseString2(str) {
+    return str.split('').reverse().join('')
+}
+
+module.exports = reverseString2;
