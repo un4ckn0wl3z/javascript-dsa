fa9b91a5162870932b326c1448d959d474ca7503
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-12 20:14:49 +0700

    feat: solved counter occurrences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,14 @@
-function countOccurrences() {}
+// function countOccurrences(str, char) {
+//     let found = 0
+//     for (i=0; i<str.length; i++) {
+//         if (str[i] === char) found++
+//     }
+//     return  found
+// }
+
+const countOccurrences = (str, char) => {
+    const splitter = str.split(char).length - 1
+    return splitter
+}
 
 module.exports = countOccurrences;
