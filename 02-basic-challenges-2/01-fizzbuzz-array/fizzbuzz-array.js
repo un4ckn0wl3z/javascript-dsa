7d0e20df551ff15915f1f18d4af36548a603bcc8
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-13 21:17:10 +0700

    feat: solved fizzbuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    const arr = []
+    for(i=1;i<=num;i++){
+        if((i % 3 === 0) && (i % 5 === 0)){
+            arr.push("FizzBuzz")
+        }else if(i % 3 === 0){
+            arr.push("Fizz")
+        }else if(i % 5 === 0){
+            arr.push("Buzz")
+        }else{
+            arr.push(i)
+        }
+    }
+
+    return arr;
+}
 
 module.exports = fizzBuzzArray;
