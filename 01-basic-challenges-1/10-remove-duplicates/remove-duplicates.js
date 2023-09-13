1515abd3ce15c0b6b15d014068a8f91c6f76ac70
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-13 21:05:11 +0700

    feat: solved remove duplicate and reverse string

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,34 @@
-function removeDuplicates() {}
+function removeDuplicates1(arr) {
+    
+    let newArr = []
 
-module.exports = removeDuplicates;
+    for(i=0;i<arr.length;i++){
+        if(!newArr.find(m => arr[i] === m)){
+            newArr.push(arr[i])
+        }
+        
+     }
+
+     return newArr;
+}
+
+function removeDuplicates2(arr) {
+    
+    let newArr = []
+
+    for(i=0;i<arr.length;i++){
+        if(!newArr.includes(arr[i])){
+            newArr.push(arr[i])
+        }
+        
+     }
+
+     return newArr;
+}
+
+function removeDuplicates3(arr) {
+    return Array.from(new Set(arr))
+}
+
+
+module.exports = removeDuplicates3;
