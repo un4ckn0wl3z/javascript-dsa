fa6d0a5988172cc5de6b6461cfc47ba03d13bab7
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-13 20:18:49 +0700

    feat: solved count-vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,13 @@
-function countVowels() {}
+function countVowels(str) {
+    const vowels = 'aeiou';
+    const strLowerCase = str.toLowerCase();
+    let vowelCount = 0;
+    strLowerCase.split('').forEach(c => {
+        if(c === vowels[0] || c === vowels[1] || c === vowels[2] || c === vowels[3] || c === vowels[4]){
+            vowelCount++;
+        }
+    })
+    return vowelCount
+}
 
 module.exports = countVowels;
