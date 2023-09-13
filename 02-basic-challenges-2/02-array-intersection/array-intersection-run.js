066fd5fc0fdb6ccc9cce8de1dde7f7e9249d8500
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-13 21:45:25 +0700

    feat: solved array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection-run.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection-run.js
@@ -1,5 +1,5 @@
 const arrayIntersection = require('./array-intersection');
 
-const result = arrayIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
+const result = arrayIntersection([1, 1, 2, 3, 4, 5], [3, 4, 1,1,1,1, 5, 6, 7]);
 
 console.log(result);
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,44 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    
+    let store = []
 
-module.exports = arrayIntersection;
+    for(i=0;i<arr1.length;i++){
+        for(j=0;j<arr2.length;j++){
+            if(arr1.includes(arr2[j])){
+                store.push(arr2[j])
+            }
+        }
+    }
+
+    return Array.from(new Set(store));
+}
+
+
+function arrayIntersection2(arr1, arr2) {
+    
+    let store = []
+
+    for(i=0;i<arr1.length;i++){
+        if(arr2.includes(arr1[i]) && !store.includes(arr1[i])){
+            store.push(arr1[i])
+        }
+    }
+
+    return store
+}
+
+
+
+function arrayIntersection3(arr1, arr2) {
+    const set1 = new Set(arr1)    
+    const set2 = new Set(arr2)    
+    const intersectArr = []
+    for(let num of set2){
+        if(set1.has(num)){
+            intersectArr.push(num)
+        }
+    }
+    return intersectArr;
+}
+
+module.exports = arrayIntersection3;
