bc3f6ff56380b23efe06d893baeef2b04ec6e189
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-18 22:31:15 +0700

    feat: update

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,30 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(nums) {
 
-module.exports = formatPhoneNumber;
+    let result = ''
+
+    for (let index = 0; index < nums.length; index++) {
+        if (index === 2){
+            result = "(" + `${nums[0]}${nums[1]}${nums[2]}) `
+        }else if (index === nums.length-1){
+            result += `${nums[3]}${nums[4]}${nums[5]}-${nums[6]}${nums[7]}${nums[8]}${nums[9]}`
+            return result
+        }
+    }
+}
+
+
+function formatPhoneNumber2(nums) {
+    let format = '(xxx) xxx-xxxx'.split('')
+    let j=0;
+    for (let index = 0; index < format.length; index++) {
+        if (format[index] === 'x') {
+            format[index] = `${nums[j]}`
+            j++
+        }
+    }
+
+    return format.join('')
+
+}
+
+module.exports = formatPhoneNumber2;
