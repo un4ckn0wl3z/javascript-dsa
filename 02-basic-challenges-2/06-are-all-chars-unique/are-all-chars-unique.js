70911741f17106c0893e9814b6451261a5f4bed3
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-18 20:48:09 +0700

    feat: solved are all chars unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,36 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+    if (str === '') return true
 
-module.exports = areAllCharactersUnique;
+    const arr = []
+    for (let index = 0; index < str.length; index++) {
+        if(index === 0) {
+            arr.push(str[index])
+        }else{
+            if(arr.includes(str[index])) {
+                return false
+            }
+        }
+        arr.push(str[index])
+    }
+
+    return true
+
+}
+
+
+function areAllCharactersUnique2(str){
+    const charCount = {}
+
+    for (let index = 0; index < str.length; index++) {
+        const char = str[index]
+        if(charCount[char]){
+            return false
+        }
+        charCount[char] = true
+    }
+
+    return true
+}
+
+
+module.exports = areAllCharactersUnique2;
