4e7115afb657856b30da2cde1fb7b25fdb66bed9
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-18 19:47:45 +0700

    feat: solved display-likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,16 @@
-function displayLikes() {}
+function displayLikes(names) {
+    
+    if(!names.length) return 'no one likes this'
+
+    if (names.length === 1) {
+        return `${names[0]} likes this`
+    } else if (names.length === 2) {
+        return `${names[0]} and ${names[1]} like this`
+    } else if (names.length === 3) {
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else {
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    }
+}
 
 module.exports = displayLikes;
