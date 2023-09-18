a66ad24af347e86dd7940e3aabb080cd29a78dcf
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-18 22:11:04 +0700

    feat: solved dice game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,27 @@
-function diceGameSimulation() {}
+function diceGameSimulation(num) {
+    const results = []
+    for (let index = 0; index < num; index++) {
+        let result = ''
+        const dice1 = Math.floor(Math.random() * 6) + 1
+        const dice2 = Math.floor(Math.random() * 6) + 1
+        sum = dice1 + dice2
+        if(sum === 7 || sum === 11){
+            result = 'win'
+        } else if (sum === 2 || sum === 3 || sum == 12){
+            result = 'lose'
+        } else {
+            result = 'roll again'
+        }
+
+        results.push({
+            dice1,
+            dice2,
+            sum,
+            result
+        })
+    }
+
+    return results;
+}
 
 module.exports = diceGameSimulation;
