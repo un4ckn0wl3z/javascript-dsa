1fc93986e08cf0f36ac885b336add16b6e64981f
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-18 20:00:09 +0700

    feat: solved find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,19 @@
-function findMissingNumber() {}
+function findMissingNumber(numbers) {
+    if (!numbers.length) return 1
+    const n = numbers.length + 1
+    const expectedSum = n * (n + 1) / 2
+
+    console.log("expectedSum", expectedSum)
+
+    let actualSum = 0
+    for (let index = 0; index < numbers.length; index++) {
+        actualSum += numbers[index];
+    }
+
+    console.log("actualSum", actualSum)
+
+
+    return expectedSum - actualSum
+}
 
 module.exports = findMissingNumber;
