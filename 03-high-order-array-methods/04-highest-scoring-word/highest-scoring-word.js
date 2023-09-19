95bf94d9fb4d8bd215ecfeeade93b1d858bbf71b
un4ckn0wl3z <admin@unknownclub.net>
2023-09-19 13:27:19 +0700

    feat: solved find highest word score with alternative method

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -23,4 +23,44 @@ function highestScoringWord(str) {
     return highest.word
 }
 
-module.exports = highestScoringWord;
+
+function highestScoringWord2(str){
+    const words = str.split(' ')
+    const scores = words.map((word) => {
+        let score = 0
+        for(const letter of word){
+            score += letter.charCodeAt(0) - 96
+        }
+        return score;
+    })
+
+let highestScore = 0;
+let highestIndex = 0;
+
+for (let index = 0; index < scores.length; index++) {
+    if(scores[index] > highestScore){
+        highestScore = scores[index]
+        highestIndex = index
+    }
+}
+
+return words[highestIndex]
+
+
+}
+
+
+function highestScoringWord3(str){
+    const words = str.split(' ')
+    const scores = words.map((word) => {
+        return Array.from(word).reduce((score, letter) => score + letter.charCodeAt(0) - 96, 0)
+    })
+
+    const highestScore = Math.max(...scores)
+    const highestIndex = scores.indexOf(highestScore)
+    return words[highestIndex]
+
+}
+
+
+module.exports = highestScoringWord3;
