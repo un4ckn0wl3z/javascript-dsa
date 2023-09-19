84bf3762c96581f1bbff045ba5d02f55818c7ae1
un4ckn0wl3z <admin@unknownclub.net>
2023-09-19 11:15:57 +0700

    feat: solved sum of square number

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,22 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares2(numbers) {
+    const sumOfSquare = numbers.reduce((total, n) => {
+        if(n % 2 === 0){
+            total += Math.pow(n, 2)
+        }
+        return total
+    }, 0)
+    return sumOfSquare
+}
 
-module.exports = sumOfEvenSquares;
+
+function sumOfEvenSquares1(numbers) {
+    const evenNumber = numbers.filter(n => n % 2 === 0)
+    const squarNumber = evenNumber.map(n => Math.pow(n, 2))
+    const reduceNumber = squarNumber.reduce((total, num) => {
+        return total + num
+    }, 0)
+
+    return reduceNumber
+}
+
+module.exports = sumOfEvenSquares2;
