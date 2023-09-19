0be3235907db075c72a8ab1cb3c4e155b2e877fe
un4ckn0wl3z <admin@unknownclub.net>
2023-09-19 13:52:50 +0700

    feat: solved valid anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,16 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+
+    const freqCount1 = str1.split('').reduce((acc, char) => {
+        acc[char] = (acc[char] || 0) + 1
+        return acc
+    }, {})
+
+    const freqCount2 = str2.split('').reduce((acc, char) => {
+        acc[char] = (acc[char] || 0) + 1
+        return acc
+    }, {})
+
+    return Object.keys(freqCount1).every((char) => freqCount1[char] === freqCount2[char])
+}
 
 module.exports = validAnagrams;
