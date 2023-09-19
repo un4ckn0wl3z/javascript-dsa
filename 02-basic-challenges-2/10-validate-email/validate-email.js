609bb14ff2c4ceab17ca1130ded651daee985b43
un4ckn0wl3z <admin@unknownclub.net>
2023-09-19 10:25:26 +0700

    feat: solved validate-email

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,25 @@
-function validateEmail() {}
+function validateEmail(email) {
+    if (email.split('@')[0] === '') return false
+    if(email.includes('@') && email.includes('.')){
+        return true
+    }
+    return false
+}
 
-module.exports = validateEmail;
+function validateEmail2(email) {
+    if(email.indexOf('@') == -1) return false
+
+    const [localPath, domain] = email.split('@');
+    if(localPath.length == 0 || domain.length < 3) return false
+
+    const domainExtension = domain.split('.');
+    if (domainExtension.length < 2 || domain[1] < 2 ) return false
+
+    return true
+
+
+}
+
+
+
+module.exports = validateEmail2;
