fe2b4ae215103d8ac47da8088f33e7ed394e5e82
un4ckn0wl3z <admin@unknownclub.net>
2023-09-19 16:02:44 +0700

    feat: solved valid-ipv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,24 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ips) => {
+    const ipv4s = ips.split('.')
+    if (!ipv4s.length || ipv4s.length > 4 || ipv4s.length < 4) return false
+    const validStart = ipv4s.every((ip) => !ip.startsWith('0'))
+    if(!validStart) return false
 
-module.exports = isValidIPv4;
+    const validRange = ipv4s.every((ip) => (parseInt(ip) >= 0 && parseInt(ip) <= 255) )
+    if(!validRange) return false
+
+    return true
+
+};
+
+const isValidIPv4v2 = (ips) => {
+    const ipv4s = ips.split('.')
+    if (!ipv4s.length || ipv4s.length > 4 || ipv4s.length < 4) return false
+
+    const valid = ipv4s.every((ip) => (parseInt(ip) >= 0 && parseInt(ip) <= 255 && parseInt(ip)+'' === ip) )
+    if(!valid) return false
+
+    return true
+};
+
+module.exports = isValidIPv4v2;
