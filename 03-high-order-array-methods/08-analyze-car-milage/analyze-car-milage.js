ad881b75ff7ffa1e1f4c04ab4a840db494938aad
un4ckn0wl3z <admin@unknownclub.net>
2023-09-19 16:41:50 +0700

    feat: solved analyze car mileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,29 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+    
+
+    const result = cars.reduce((result, car, index) => {
+
+        if(car.mileage > result.highestMileageCar.mileage){
+            result.highestMileageCar = car
+        }
+
+        if(car.mileage < result.lowestMileageCar.mileage){
+            result.lowestMileageCar = car
+        }
+
+        result.totalMileage += car.mileage
+        result.averageMileage = parseFloat((result.totalMileage / ( index +1 )).toFixed(2))
+        return result
+    }, {
+        averageMileage: 0,
+        highestMileageCar: {
+            mileage: 0
+        },
+        lowestMileageCar: cars[0],
+        totalMileage: 0
+    })
+
+   return result
+}
 
 module.exports = analyzeCarMileage;
