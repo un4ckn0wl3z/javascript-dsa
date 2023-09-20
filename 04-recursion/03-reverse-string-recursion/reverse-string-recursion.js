2d41c604c2f383ffe2d341fec65a9e6370e57cdd
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-20 11:57:25 +0700

    feat: solved recursive unwind

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,28 @@
-function reverseString() {}
+function reverseString(str) {
+    
+    // Base Case
+    if(str === ''){
+        return ''
+    }
+    // Recursive Case
+    const reversed = reverseString(str.substring(1))
+
+    // Unwind
+    return reversed + str.charAt(0)
+
+}
 
 module.exports = reverseString;
+
+
+/**
+ * 
+ * reverseString(str.substring(ello))
+ * reverseString(str.substring(llo))
+ * reverseString(str.substring(lo))
+ * reverseString(str.substring(o))
+ * reverseString(str.substring(''))
+ * 
+ * unwind
+ * ''+'o'+'l'+'l'+'e'+'h'
+ */
