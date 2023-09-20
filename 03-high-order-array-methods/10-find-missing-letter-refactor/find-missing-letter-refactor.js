b1e77dc3c41897295c7b4ff12b6cd01a4ab8c4e0
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-20 10:17:30 +0700

    feat: solved missing letter refactor

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,16 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+    let start = arr[0].charCodeAt(0) // get charcode from first index
+    const missingCharCode = arr
+    .map((char) => char.charCodeAt(0)) // transform character to charcode
+    .find((current) => { // loop through transformed charcode
+        if(current - start > 1 ){ // if current - start > 1 it mean found skipped character
+            return true // tell a find mehtod "Yes, I'm found" and return back current charcode
+        }
+        start = current // change start charcode to current (it's will previous in next)
+        return false // tell a find method "Nahh, not found yet"
+    })
+
+    return missingCharCode ? String.fromCharCode(missingCharCode - 1) : ''
+}
 
 module.exports = findMissingLetter;
