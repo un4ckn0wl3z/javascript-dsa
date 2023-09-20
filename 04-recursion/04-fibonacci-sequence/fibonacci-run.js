c07ad71ec92ece4c2ca15d6fbe2a2a8a6f708df1
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-20 12:33:27 +0700

    feat: solved fibb

--- 04-recursion/04-fibonacci-sequence/fibonacci-run.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci-run.js
@@ -1,5 +1,5 @@
 const fibonacci = require('./fibonacci');
 
-const result = fibonacci(8);
+const result = fibonacci(100);
 
 console.log(result);
--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,6 @@
-function fibonacci() {}
+function fibonacci(index) {
+    if (index < 2) return index
+    return fibonacci( index - 1) + fibonacci( index - 2)
+}
 
 module.exports = fibonacci;
