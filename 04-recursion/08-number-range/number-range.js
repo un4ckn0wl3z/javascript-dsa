600c3b3ba1600d27f26ee7e0b2ff6b6779ae5e2a
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-23 18:25:11 +0700

    feat: solved number-range with recursive

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,15 @@
-function numberRange() {}
+function numberRange(start, end) {
+
+    // base case
+    if (start === end) return [start]
+
+    // recursive case
+    const arr = []
+    arr.push(start)
+    const new_ = numberRange(start + 1, end) 
+
+    // unwind
+    return arr.concat(new_)
+}
 
 module.exports = numberRange;
