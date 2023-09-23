7aa533828586d70e05b447c622ac488ed214d129
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-23 20:17:35 +0700

    feat: solved O(log n)

--- 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
+++ 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
@@ -5,3 +5,28 @@ Logarithmic time means that the time required to complete a function is proporti
 
 */
 
+// with O(n)
+function findPower(base, expo){
+    if (expo === 0) return 1
+    return base * findPower(base, --expo);
+}
+
+console.time('findPower')
+findPower(2, 10000)
+console.timeEnd('findPower')
+
+
+// with O(log n)
+function findPowerLogn(base, expo){
+    if (expo === 0) return 1
+    if(expo % 2 === 0){
+        const half = findPowerLogn(base, expo / 2)
+        return half * half
+    } 
+    const half = findPowerLogn(base, (expo - 1) / 2);
+    return base * half * half
+}
+
+console.time('findPowerLogn')
+findPowerLogn(2, 10000)
+console.timeEnd('findPowerLogn')
