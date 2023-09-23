00d468e34bf7b1dee2a02dba2981f72a86b2d38a
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-23 21:16:00 +0700

    feat: solved max sub array

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,13 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+    let maxSum = -Infinity;
+    for (let i = 0; i <= arr.length - k; i++) {
+        let currentSum = 0;
+        for (let j = i; j < i + k; j++) {
+            currentSum += arr[j]
+        }
+        maxSum = Math.max(maxSum, currentSum)
+    }
+    return maxSum
+}
 
 module.exports = maxSubarraySum;
