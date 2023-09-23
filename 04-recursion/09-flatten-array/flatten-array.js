891320aebbfd16d865ea740803870d2a5d562eaa
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-23 18:50:59 +0700

    feat: solved flatten array

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,14 @@
-function flattenArray() {}
+function flattenArray(arr) {
+    let result = []
+    for (const item of arr) { // base case is when end of loop (last array index) [implicit]
+        if(Array.isArray(item)){
+            result = result.concat(flattenArray(item)) // recursive case
+        } else {
+            result.push(item)
+        }
+    }
+    return result; // unwind
+}
+
 
 module.exports = flattenArray;
