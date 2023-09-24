61f3c4bbc2b31ac02c58529b85c77136979895e3
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-24 18:20:20 +0700

    feat: solved wordFrequencyCounter

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,14 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(words) {
+    
+    if (words === '') return new Map()
+
+    const wordsArr = words.toLowerCase().replaceAll('.', '').replaceAll(',', '').split(' ')
+    const wordFreq = new Map()
+    for (const item of wordsArr) {
+        wordFreq.set(item, (wordFreq.get(item) || 0) + 1)
+    }
+    
+    return wordFreq
+}
 
 module.exports = wordFrequencyCounter;
