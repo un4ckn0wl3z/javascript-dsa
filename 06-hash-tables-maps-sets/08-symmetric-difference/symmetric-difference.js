ee6d25401f9148b0abd86d7240a401f2ad426580
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-24 19:40:06 +0700

    feat: symmetricDifference

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,17 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+    const newArr = []
+    const setArr1 = new Set(arr1)
+    const setArr2 = new Set(arr2)
+
+    for (const item of setArr1) {
+        if (!setArr2.has(item)) newArr.push(item)
+    }
+
+    for (const item of arr2) {
+        if (!setArr1.has(item)) newArr.push(item)
+    }
+
+    return newArr
+}
 
 module.exports = symmetricDifference;
