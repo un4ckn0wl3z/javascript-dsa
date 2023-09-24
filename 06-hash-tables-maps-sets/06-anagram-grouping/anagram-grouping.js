8af02bb76d246a0ffa34c06a6c71a6f86af59341
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-24 19:03:36 +0700

    feat: solved anagramGrouping

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,21 @@
-function anagramGrouping() {}
+function anagramGrouping(arr) {
+    const result = new Map()
+
+    for (const item of arr) {
+        const sorted = item.split('').sort().join('')
+
+        if(!result.has(sorted)){
+            result.set(sorted, [item])
+        } else {
+            const previousArr = result.get(sorted)
+            previousArr.push(item)
+            result.set(sorted, previousArr)
+        }
+
+    }
+
+    return result.values()
+    
+}
 
 module.exports = anagramGrouping;
