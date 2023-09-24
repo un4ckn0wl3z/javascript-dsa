6929a9a0d79c8edf8861fad062a3cf8c856c5cef
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-24 18:24:55 +0700

    feat: solved phoneNumberDirectory

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,10 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+    const phoneDir = new Map();
+    for (const item of arr) {
+        const [name, phone] = item.split(':')
+        phoneDir.set(name, phone)
+    }
+    return phoneDir;
+}
 
 module.exports = phoneNumberDirectory;
