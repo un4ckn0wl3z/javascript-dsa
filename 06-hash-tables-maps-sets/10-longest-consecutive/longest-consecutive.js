f4d6c5cdf76ad28a050688dde3ceb523b9d2d2af
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-25 19:37:38 +0700

    feat: solved longestConsecutiveSequence

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,21 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(arr) {
+    const arrSet = new Set(arr)
+    let longestSeq = 0;
+    for (const num of arrSet) {
+        if (!arrSet.has(num-1)){
+            let current = num
+            let currentSeq = 1
+
+            while (arrSet.has(current + 1)) {
+                current++
+                currentSeq++
+            }
+            longestSeq = Math.max(longestSeq, currentSeq)
+        }
+    }
+
+    return longestSeq
+
+}
 
 module.exports = longestConsecutiveSequence;
