a6b62e3e8ee7d91be9722b7b1008a825c17b63bb
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 12:46:10 +0700

    feat: solved enqueue

--- 07-stacks-queues-linked-lists/06-queue-implementation/queue-run.js
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue-run.js
@@ -1,12 +1,25 @@
 const Queue = require('./queue');
 
 const queue = new Queue();
-console.log(queue.isEmpty());
-console.log(queue.isFull());
-console.log(queue.enqueue(1));
-console.log(queue.enqueue(2));
-console.log(queue.enqueue(3));
-console.log(queue.peek());
-console.log(queue.dequeue());
-console.log(queue.peek());
-console.log(queue.getLength());
+
+
+queue.enqueue('a')
+queue.enqueue('b')
+queue.enqueue('c')
+
+console.log(queue)
+console.log(queue.peek())
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+console.log(queue)
+
+// console.log(queue.isEmpty());
+// console.log(queue.isFull());
+// console.log(queue.enqueue(1));
+// console.log(queue.enqueue(2));
+// console.log(queue.enqueue(3));
+// console.log(queue.peek());
+// console.log(queue.dequeue());
+// console.log(queue.peek());
+// console.log(queue.getLength());
--- 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
@@ -1,3 +1,44 @@
-class Queue {}
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.head = 0
+        this.tail = 0
+        this.maxSize = 100
+    }
 
-module.exports = Queue;
+
+    dequeue() {
+        const item = this.queue[this.head]
+        this.head++
+        return item
+    }
+
+
+    peek(){
+        return this.queue[this.head]
+    }
+
+    enqueue(item) {
+        if (this.isFull()) return false
+
+        this.queue[this.tail] = item
+        this.tail++
+        return true
+    }
+
+    getLength() {
+        return this.tail - this.head
+    }
+
+    isEmpty() {
+        return this.getLength() === 0
+    }
+
+    isFull() {
+        return this.getLength() === this.maxSize
+    }
+
+}
+
+
+module.exports = Queue
