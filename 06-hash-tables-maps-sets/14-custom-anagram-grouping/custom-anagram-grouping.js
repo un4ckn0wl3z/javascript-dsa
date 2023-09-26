fd9c9fbb79b40976916bbdadbfa5326ab77302e3
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 09:44:46 +0700

    feat: solved anagramGrouping with custom hashmap

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,24 @@
 const HashTable = require('./HashTable');
 
-function anagramGrouping() {}
+function anagramGrouping(arr) {
+
+    const result = new HashTable()
+
+    for (const item of arr) {
+        const sorted = item.split('').sort().join('')
+
+        if(!result.has(sorted)){
+            result.set(sorted, [item])
+        } else {
+            const previousArr = result.get(sorted)
+            previousArr.push(item)
+            result.set(sorted, previousArr)
+        }
+
+    }
+
+    return Array.from(result.getValues())
+
+}
 
 module.exports = anagramGrouping;
