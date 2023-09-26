efa2ba37fdb07a63aae9ef88613f0862eda7c782
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 10:09:17 +0700

    feat: solved custom stack

--- 07-stacks-queues-linked-lists/02-stack-implementation/stack-run.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack-run.js
@@ -1,2 +1,15 @@
 const Stack = require('./stack');
 
+const stack = new Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+console.log(stack)
+console.log('peek: ',stack.peek())
+console.log(stack.pop())
+console.log(stack.pop())
+console.log(stack.pop())
+console.log(stack.pop())
+console.log(stack)
--- 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
@@ -1,3 +1,42 @@
-class Stack {}
+class Stack {
+
+    constructor() {
+        this.maxSize = 100
+        this.stack = []
+        this.top = -1
+    }
+
+    push(value) {
+        if (this.isFull()) {
+            return false
+        }
+       
+        this.top++
+        this.stack[this.top] = value
+        return true
+    }
+
+    pop() {
+        if(this.isEmpty()) return null
+        // const valueToReturn = this.stack[this.top]
+        this.top--
+        return this.stack.pop()
+    }
+
+    peek() {
+        if(this.isEmpty()) return null
+        return this.stack[this.top]
+    }
+
+    isFull() {
+        return this.top === this.maxSize - 1
+    }
+
+    isEmpty(){
+        return this.top === -1
+    }
+
+
+}
 
 module.exports = Stack;
