b681d350186f42cec1cfba7f09ee5586514d9c6e
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 10:25:47 +0700

    feat: solved balancedParenthesis

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -1,5 +1,20 @@
 const Stack = require('./stack');
 
-function balancedParenthesis() {}
+function balancedParenthesis(str) {
+    
+    const stack = new Stack()
+
+    for (let index = 0; index < str.length; index++) {
+        if (str[index] === '(') {
+            stack.push(str[index])
+        } 
+        if (str[index] === ')') {
+            if(stack.isEmpty()) return false
+            stack.pop()
+        } 
+    }
+
+    return stack.isEmpty()
+}
 
 module.exports = balancedParenthesis;
