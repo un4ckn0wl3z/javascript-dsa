beb7f0059474034aa8f42d14f33194ef01c0bd9d
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 13:51:31 +0700

    feat: solved isPalindromeQueueStack

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,25 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+    const cleanWord = str.replaceAll(' ', '').replaceAll(',', '').replaceAll(':', '').replaceAll('.', '').toLowerCase()
+    const stack = new Stack()
+    const queue = new Queue()
+
+    for (let i = 0; i < cleanWord.length; i++) {
+        queue.enqueue(cleanWord[i])
+        stack.push(cleanWord[i])
+    }
+
+    while(!queue.isEmpty() && !stack.isEmpty()){
+        if(queue.dequeue() === stack.pop()){
+            continue;
+        }else {
+            return false
+        }
+    }
+
+    return true
+}
 
 module.exports = isPalindromeQueueStack;
