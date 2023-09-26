6df8c1853c0b1d9e7ebce531106dc8d978028d19
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 12:55:19 +0700

    feat: solved reverseStringQueue

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,18 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+    let result = ''
+    const q = new Queue()
+
+    for (let index = str.length - 1; index >= 0; index--) {
+        q.enqueue(str[index])
+    }
+   
+    while(!q.isEmpty()){
+        result += q.dequeue() 
+    }
+
+    return result
+};
 
 module.exports = reverseStringWithQueue;
