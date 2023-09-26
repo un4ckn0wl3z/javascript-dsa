c959b31f30fdfc0901fe6a4325aba031e2896650
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 10:17:33 +0700

    feat: solved reverseStringStack

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,18 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+    const stack = new Stack()
+    let result = ''
+
+    for (let index = 0; index < str.length; index++) {
+        stack.push(str[index])
+    }
+
+    while(!stack.isEmpty()){
+        result += stack.pop()
+    }
+    
+    return result
+}
 
 module.exports = reverseStringStack;
