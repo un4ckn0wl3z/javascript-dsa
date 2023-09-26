93a5c9d262649b4c9e0c7e662eef0b315f8e07db
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 21:27:21 +0700

    feat: solved reverse string with custom linkedlist

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,18 @@
 const { LinkedList } = require('./linked-list');
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(words) {
+    const cleanWords = words.replaceAll('.', '').replaceAll(',', '').replaceAll('!', '')
+    const linkedList = new LinkedList()
+    let result = ''
+    for (let index = cleanWords.length - 1; index >= 0; index--) {
+        linkedList.add(cleanWords[index])
+    }
+    let current = linkedList.head
+    while (current != null) {
+        result += current.data
+        current = current.next
+    }
+    return result
+}
 
 module.exports = reverseStringLinkedList;
