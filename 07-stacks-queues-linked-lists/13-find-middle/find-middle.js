ee3ecc47bf55c1f19b2d2ddf8d929eb72c860318
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-26 22:19:41 +0700

    feat: solved findMiddle

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -1,3 +1,19 @@
-function findMiddle() {}
+function findMiddle(list) {
+    let slow = list.head
+    let fast = list.head
+
+    let prev = null
+
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next
+        prev = slow
+        slow = slow.next
+    }
+
+    if (fast === null) {
+        return prev.next
+    }
+    return slow
+}
 
 module.exports = findMiddle;
