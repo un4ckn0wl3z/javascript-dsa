acf5fecc6e775f3dc438d81a22b7a9574d71d79d
un4ckn0wl3z <admin@unknownclub.net>
2023-09-28 10:23:20 +0700

    feat: implement DoublyLinkedList part 1

--- 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
+++ 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
@@ -1,2 +1,12 @@
 const DoublyLinkedList = require('./doubly-linked-list');
 
+const list = new DoublyLinkedList()
+list.append(100)
+list.append(200)
+list.append(300)
+list.append(400)
+list.prepend(99)
+
+list.insertAt(1, 99.5)
+
+list.printAll()
--- 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list.js
+++ 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list.js
@@ -1,5 +1,80 @@
-function Node() {}
+function Node(data) {
+    this.data = data
+    this.next = null
+    this.prev = null
+}
 
-function DoublyLinkedList() {}
+function DoublyLinkedList() {
+    this.head = null
+    this.tail = null
+    this.length = 0 
+}
+
+DoublyLinkedList.prototype.append = function(data) {
+    const newNode = new Node(data)
+
+    if (!this.head) {
+        this.head = newNode
+        this.tail = newNode
+    } else {
+        newNode.prev = this.tail
+        this.tail.next = newNode
+        this.tail = newNode
+    }
+
+    this.length++
+}
+
+DoublyLinkedList.prototype.prepend = function(data) {
+    const newNode = new Node(data)
+
+    if (!this.head) {
+        this.head = newNode
+        this.tail = newNode
+    } else {
+        newNode.next = this.head
+        this.head.prev = newNode
+        this.head = newNode
+    }
+
+    this.length++
+}
+
+DoublyLinkedList.prototype.insertAt = function(index, data) {
+    if(index < 0 || index > this.length) {
+        return null
+    }
+
+    if (index === 0) {
+        return this.prepend(data)
+    }
+
+    if (index === this.length) {
+        return this.append(data)
+    }
+
+    const newNode = new Node(data)
+    let currentNode = this.head
+
+    for (let i = 0; i < index -1; i++) {
+        currentNode = currentNode.next
+    }
+
+    newNode.next = currentNode.next
+    newNode.prev = currentNode
+    currentNode.next.prev = newNode
+    currentNode.next = newNode
+    this.length++
+}
+
+
+DoublyLinkedList.prototype.printAll = function() {
+    let current = this.head
+    while (current) {
+        console.log(current.data)
+        current = current.next
+    }
+
+}
 
 module.exports = DoublyLinkedList;
