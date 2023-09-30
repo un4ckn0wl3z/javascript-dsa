31755f8e4e6b798aabc46cf66fdfcc0b7e03989f
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-30 19:37:12 +0700

    feat: solved breadthFirstTraversal

--- 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -1,8 +1,43 @@
 const Queue = require('./queue');
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+}
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(root) {
+
+  if (!root) return []
+  
+  const result = []
+  const queue = []
+
+  queue.push(root)
+
+  while (queue.length > 0) {
+
+    const current = queue.shift()
+    result.push(current.data)
+
+    if (current.left) {
+      queue.push(current.left)
+    }
+
+    if (current.right) {
+      queue.push(current.right)
+    }
+
+
+  }
+
+
+  return result;
+
+
+}
 
 module.exports = {
   Node,
