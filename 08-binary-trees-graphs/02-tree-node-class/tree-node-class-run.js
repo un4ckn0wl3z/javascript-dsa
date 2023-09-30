275326034f5277331e56d49bf0248f0e0fcf2546
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-30 12:53:52 +0700

    feat: solved TreeNode

--- 08-binary-trees-graphs/02-tree-node-class/tree-node-class-run.js
+++ 08-binary-trees-graphs/02-tree-node-class/tree-node-class-run.js
@@ -1,1 +1,19 @@
 const TreeNode = require('./tree-node-class');
+
+const a = new TreeNode('a')
+const b = new TreeNode('b')
+const c = new TreeNode('c')
+const d = new TreeNode('d')
+const e = new TreeNode('e')
+const f = new TreeNode('f')
+
+a.left = b
+a.right = c
+
+b.left = d
+b.right = e
+
+c.right = f
+
+console.log(a)
+ 
