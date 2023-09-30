f47280b2020813f1953bf13d32a59c06a71fa807
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-09-30 20:32:19 +0700

    feat: solved find max tree depth

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth-run.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth-run.js
@@ -5,3 +5,21 @@ const { Node, maxDepth } = require('./maximum-depth');
 //                          9  20
 //                             / \
 //                            15  7
+
+const root = new Node(3)
+const node9 = new Node(9)
+const node20 = new Node(20)
+const node15 = new Node(15)
+const node7 = new Node(73)
+
+
+root.left = node9
+root.right = node20
+
+node20.left = node15
+node20.right = node7
+
+
+const result = maxDepth(root)
+
+console.log(result)
--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -1,6 +1,21 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+}
 
-function maxDepth() {}
+function maxDepth(root) {
+  if (!root) return []
+
+  const leftDepth = maxDepth(root.left)
+  const rightDepth = maxDepth(root.right)
+
+  const max = Math.max(leftDepth, rightDepth) + 1
+  return max
+
+}
 
 module.exports = {
   maxDepth,
