28f793dc7d151ae0e9c264dbce1714373d847d36
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-10-01 12:59:52 +0700

    feat: solved isValidBST

--- 08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
+++ 08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
@@ -7,3 +7,19 @@ const { Node, isValidBST } = require('./validate-bst');
 //    / \
 //   2   6
 
+const root = new Node(8)
+const node4 = new Node(4)
+const node2 = new Node(2)
+const node6 = new Node(6)
+const node10 = new Node(10)
+
+root.left =node4
+root.right = node10
+
+node4.left = node2
+node4.right = node6
+
+const result = isValidBST(root)
+
+console.log(result)
+
--- 08-binary-trees-graphs/09-validate-bst/validate-bst.js
+++ 08-binary-trees-graphs/09-validate-bst/validate-bst.js
@@ -6,6 +6,27 @@ class Node {
   }
 }
 
-function isValidBST() {}
+function isValidBST(root) {
+  
+  function isValidSubTree(node, min, max) {
+    if (!node) return true
+    
+    if (
+      (min !== null && node.value <= min) ||
+      (max !== null && node.value >= max)
+    ) {
+      return false
+    }
+
+    return (
+      isValidSubTree(node.left, min, node.value) &&
+      isValidSubTree(node.right, node.value, max)
+    )
+    
+  }
+
+  return isValidSubTree(root, null, null)
+
+}
 
 module.exports = { Node, isValidBST };
