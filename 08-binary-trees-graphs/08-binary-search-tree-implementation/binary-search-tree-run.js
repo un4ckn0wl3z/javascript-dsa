3f1b2f9dfca1f6e9a4ee28cdc8b1f0511594cb5d
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-10-01 12:22:37 +0700

    feat: implement BinarySearchTree

--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
@@ -7,3 +7,21 @@ const { Node, BinarySearchTree } = require('./binary-search-tree');
 //    /
 //   2
 
+const bst = new BinarySearchTree()
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(2)
+
+// bst.insert(2)
+// bst.insert(3)
+
+bst.printTree()
+
+bst.remove(5)
+
+console.log('*'.repeat(20))
+
+bst.printTree()
+
+// console.log(bst.lookup(5))
--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
@@ -1,5 +1,106 @@
-class Node {}
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
 
-class BinarySearchTree {}
+class BinarySearchTree {
+    
+    constructor() {
+        this.root = null
+    }
+
+    insert(value) {
+        const newNode = new Node(value)
+        if(!this.root){
+            this.root = newNode
+        } else {
+            let currentNode = this.root
+            while (true) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode
+                        return this
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    if (!currentNode.right) {
+                        currentNode.right = newNode
+                        return this
+                    }
+                    currentNode = currentNode.right
+                }
+            }
+        }
+    }
+
+
+    lookup(value) {
+        let currentNode = this.root
+        if (!currentNode) return null
+
+        while (currentNode) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right
+            } else if (value === currentNode.value) {
+                return currentNode
+            }
+        }
+
+        return null
+    }
+
+    remove (value) {
+        const removeNode = (node, value) => {
+            if (!node) return null
+
+            if (value < node.value) {
+                node.left = removeNode(node.left, value)
+                return node
+            } else if (value > node.value) {
+                node.right = removeNode(node.right, value)
+                return node
+            } else {
+                
+                if (node.left === null) { // case 1:
+                    return node.right
+                } else if (node.right === null) { // case 2:
+                    return node.left
+                }
+
+
+                let tempNode = node.right;
+                while(tempNode.left !== null) {
+                    tempNode = tempNode.left
+                }
+                node.value = tempNode.value
+                node.right = removeNode(node.right, tempNode.value)
+                return node
+            }
+
+        }
+
+        this.root = removeNode(this.root, value)
+    }
+
+    printTree () {
+        const printNode = (node) => {
+            if (!node) return
+
+            printNode(node.left)
+            console.log(node.value)
+            printNode(node.right)
+        }
+
+        printNode(this.root)
+    }
+
+
+
+}
 
 module.exports = { Node, BinarySearchTree };
