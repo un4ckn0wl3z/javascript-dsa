2f45fc9f57a838eeaee16d3fae58f0b03f314c29
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-10-02 21:12:05 +0700

    solved depthFirstTraversal graph

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,5 +1,30 @@
 const Stack = require('./stack');
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(graph, startingVertex) {
+    if (!graph.adjacencyList[startingVertex]) return []
+
+    const visited = {}
+    const stack = new Stack()
+    const result = []
+
+    stack.push(startingVertex)
+    visited[startingVertex] = true
+
+    while (!stack.isEmpty()) {
+        const currentVertex = stack.pop();
+        result.push(currentVertex)
+
+        graph.adjacencyList[currentVertex].forEach(neighbor =>  {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true
+                stack.push(neighbor)
+            }
+        })
+
+    }
+
+    return result;
+
+}
 
 module.exports = depthFirstTraversal;
