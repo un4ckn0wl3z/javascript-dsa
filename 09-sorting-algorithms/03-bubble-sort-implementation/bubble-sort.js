a4f5ef13387a60298b8f5b87985195e3441a6a98
un4ckn0wl3z <admin@unknownclub.net>
2023-10-04 11:26:48 +0700

    feat: implement bubble sort

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,8 +1,29 @@
 // const array = [5, 4, 2, 1];
 
+
 function bubbleSort(array) {
+
+    let swapped = false;
+
+    while(!swapped) {
+        swapped = true
+        for (let index = 0; index < array.length; index++) {
+            if (array[index] > array[index + 1]) {
+                const temp = array[index]
+                array[index] = array[index + 1]
+                array[index + 1] = temp
+                swapped = false
+            }
+            
+        }
+    }
+
+    return array
+}
+
+function bubbleSort2(array) {
     for (let i = 0; i < array.length; i++) {
-        console.log(array)
+        //console.log(array)
         for (let j = 0; j < array.length - i - 1; j++) {
             if (array[j] > array[j + 1]) {
                 const temp = array[j]
