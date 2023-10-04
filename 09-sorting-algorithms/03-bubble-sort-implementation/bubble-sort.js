2be9b1f09d821e165d34db4bb3c888cc6dbdb4c0
un4ckn0wl3z <admin@unknownclub.net>
2023-10-04 11:16:43 +0700

    feat: implement bubble sort

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,3 +1,88 @@
-function bubbleSort() {}
+// const array = [5, 4, 2, 1];
+
+function bubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array)
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+    }
+
+    return array
+}
+/**
+ *      [ 5, 4, 2, 1 ]
+ * 
+        for (let j = 0; j < 3; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+
+        end: [ 4, 5, 2, 1 ]
+
+        for (let j = 1; j < 3; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+
+         end: [ 4, 2, 5, 1 ]
+
+        for (let j = 2; j < 3; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+        
+         end: [ 4, 2, 1, 5 ]
+
+
+        for (let j = 0; j < 2; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+
+         end: [ 2, 4, 1, 5 ]
+
+        for (let j = 1; j < 2; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+
+         end: [ 2, 1, 4, 5 ]
+
+
+        for (let j = 0; j < 1; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+
+        [ 1, 2, 4, 5 ]
+ * 
+ * 
+ * 
+ * 
+ */
+
 
 module.exports = bubbleSort;
