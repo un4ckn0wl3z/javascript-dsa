882cb3f19c1150ecadb400cab6f162797a38c7ec
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-10-05 10:51:18 +0700

    feat: solved selectionSort

--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -1,3 +1,20 @@
-function selectionSort() {}
+
+//  [4, 3, 2, 10, 12, 1, 5, 6];
+function selectionSort(array) {
+    for (let i = 0; i < array.length - 1; i++) {       
+        let minIndex = i;
+        for (let j = i+1; j < array.length; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j
+            }
+        } 
+
+        if (minIndex !== i) {
+            [array[i], array[minIndex]] = [array[minIndex], array[i]]
+        }
+    }
+
+    return array
+}
 
 module.exports = selectionSort;
