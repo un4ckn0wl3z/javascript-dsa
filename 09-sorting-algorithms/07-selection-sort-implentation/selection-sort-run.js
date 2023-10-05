bc650d9db61171e3b951a7a3daff7b02fa4f0109
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2023-10-05 10:57:14 +0700

    feat: self explain swap with desctruction

--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort-run.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort-run.js
@@ -5,3 +5,10 @@ const array = [4, 3, 2, 10, 12, 1, 5, 6];
 const result = selectionSort(array);
 
 console.log(result);
+
+
+// const data = ['a','b','c','d','e','f']
+// console.log(data)
+
+// console.log([data[0],data[1]] = [data[1],data[0]])
+// console.log(data)
