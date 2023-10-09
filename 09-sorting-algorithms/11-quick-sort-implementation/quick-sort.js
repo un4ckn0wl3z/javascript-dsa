965476d2876baf6e3916b4bac13da2745c05df00
un4ckn0wl3z <admin@unknownclub.net>
2023-10-09 10:22:23 +0700

    feat: solved quickSort

--- 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
+++ 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
@@ -1,4 +1,20 @@
-function quickSort() {
+function quickSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    const pivot = arr[arr.length - 1]
+
+    const left = []
+    const right = []
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+
+     return [...quickSort(left), pivot, ...quickSort(right)]
 
 }
 
